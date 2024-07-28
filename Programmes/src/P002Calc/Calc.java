b1633f6112045b47064a74190b410b21c548a5e2
dib-a <dib.ahmad.000@gmail.com>
2024-07-28 21:17:24 +0200

    P002Calc - split the TODOs

--- Programmes/src/P002Calc/Calc.java
+++ Programmes/src/P002Calc/Calc.java
@@ -2,23 +2,26 @@ package P002Calc;
 
 public class Calc {
 
-    //TODO: Das Programm soll 2 Zahlen addieren, subtrahieren, multiplizieren und dividieren können
     public static void main(String[] args) {
         Calc calc = new Calc();
     }
 
+    //TODO: Addiere 2 Zahlen
     public double add(double a, double b) {
         return 0;
     }
 
+    //TODO: Subtrahiere 2 Zahlen
     public double sub(double a) {
         return a;
     }
 
+    //TODO: Multipliziere 2 Zahlen
     public double mul(double b) {
         return 0;
     }
 
+    //TODO: Dividiere 2 Zahlen
     public double div() {
         return 0;
     }
