b8331d5e5a8d36aa7e7f7dfffae3772fc511d59f
dib-a <dib.ahmad.000@gmail.com>
2024-07-28 17:23:21 +0200

    001-HelloWorld

---
+++ 001-HelloWorld/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+
+    //TODO: Das Programm soll "Hello World!" ausgeben
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.sayHello();
+    }
+}
---
+++ 001-HelloWorld/test/MainTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testAdd() {
+        Main main = new Main();
+        main.sayHello();
+        assertEquals("Hello World!\n", outContent.toString());
+    }
+}
