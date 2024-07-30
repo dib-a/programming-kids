de79b5fcd81d8c4091a0616ba794380a7cf53750
dib-a <dib.ahmad.000@gmail.com>
2024-07-30 20:07:32 +0200

    P003ConditionIf - added comments

--- Programmes/src/P003ConditionIf/ConditionIf.java
+++ Programmes/src/P003ConditionIf/ConditionIf.java
@@ -2,12 +2,13 @@ package P003ConditionIf;
 
 public class ConditionIf {
     public static void main(String[] args) {
+        // Hier wird ein Object der Klasse ConditionIf erzeugt
         ConditionIf condition = new ConditionIf();
 
 
     }
 
-    //TODO: Es soll überprüft werden, ob das Alter über 18 ist
+    //TODO: Es soll überprüft werden, ob das Alter größer oder gleich 18 ist
     public boolean checkAge(int age) {
         return false;
     }
