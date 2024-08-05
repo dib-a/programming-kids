0c579a7e50c935732640f063a7fba4fdfccacbe9
dib-a <dib.ahmad.000@gmail.com>
2024-08-05 19:59:05 +0200

    fixed tests

--- Programmes/test/P005LoopWhile/LoopWhileTest.java
+++ Programmes/test/P005LoopWhile/LoopWhileTest.java
@@ -8,8 +8,9 @@ public class LoopWhileTest {
     @Test
     public void testCalculateSum() {
         LoopWhile calculator = new LoopWhile();
-        int erwarteteSumme = 2550;
+        int erwarteteSumme = 5050;
         int actualSum = calculator.calculateSum();
-        assertEquals("Die berechnete Summe der geraden Zahlen von 1 bis 100 ist nicht korrekt", erwarteteSumme, actualSum);
+        assertEquals("\nDie berechnete Summe der Zahlen von 1 bis 100 ist nicht korrekt\nEs Ergebnis sollte " +
+                erwarteteSumme + " sein, war aber " + actualSum + "; ", erwarteteSumme, actualSum);
     }
 }
