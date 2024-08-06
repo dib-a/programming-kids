faed840a082c37eeeb17bad8d161d770955cbeee
dib-a <dib.ahmad.000@gmail.com>
2024-08-07 00:25:38 +0200

    rename loopFor object

--- Programmes/src/P006LoopFor/LoopFor.java
+++ Programmes/src/P006LoopFor/LoopFor.java
@@ -2,13 +2,14 @@ package P006LoopFor;
 
 public class LoopFor {
     public static void main(String[] args) {
-        LoopFor loopWhile = new LoopFor();
+        LoopFor loopFor = new LoopFor();
         // Hier nichts verändern
-        System.out.println(loopWhile.calculateSum());
+        System.out.println(loopFor.calculateSum());
     }
 
-    /*TODO: Bilde die Summe von 1 bis 100 (d.h 1+2+3+...+98+99+100)
-       Verwende eine for-schleife
+    /*
+    TODO: Bilde die Summe von 1 bis 100 (d.h 1+2+3+...+98+99+100)
+     Verwende eine for-schleife
      */
     public int calculateSum() {
         return 0;
