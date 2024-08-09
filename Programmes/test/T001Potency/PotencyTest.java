431f694e702c622833557d4df4dad1ae746c21f1
dib-a <dib.ahmad.000@gmail.com>
2024-08-09 22:31:36 +0200

    T001Potency: fixed tests

--- Programmes/test/T001Potency/PotencyTest.java
+++ Programmes/test/T001Potency/PotencyTest.java
@@ -9,38 +9,64 @@ public class PotencyTest {
     @Test
     public void testCalculatePotencyWithPositiveExponents() {
         Potency potency = new Potency();
-        assertEquals(1, potency.calculatePotency(5, 0));  // Test für a^0 = 1
-        assertEquals(5, potency.calculatePotency(5, 1));  // Test für a^1 = a
-        assertEquals(25, potency.calculatePotency(5, 2)); // Test für 5^2 = 25
-        assertEquals(125, potency.calculatePotency(5, 3)); // Test für 5^3 = 125
+        assertEquals("\nEs sollte 1 ausgegeben werden, es wurde aber " +
+                        potency.calculatePotency(5, 0) + " ausgegeben",
+                1, potency.calculatePotency(5, 0));
+        assertEquals("\nEs sollte 5 ausgegeben werden, es wurde aber " +
+                        potency.calculatePotency(5, 1) + " ausgegeben",
+                5, potency.calculatePotency(5, 1));
+        assertEquals("\nEs sollte 25 ausgegeben werden, es wurde aber " +
+                        potency.calculatePotency(5, 2) + " ausgegeben",
+                25, potency.calculatePotency(5, 2));
+        assertEquals("\nEs sollte 125 ausgegeben werden, es wurde aber " +
+                        potency.calculatePotency(5, 3) + " ausgegeben",
+                125, potency.calculatePotency(5, 3));
     }
 
     @Test
     public void testCalculatePotencyWithNegativeExponents() {
         Potency potency = new Potency();
-        assertEquals(0, potency.calculatePotency(2, -1));  // Test für negative Exponenten, erwartet wird 0
-        assertEquals(0, potency.calculatePotency(5, -3));  // Test für negative Exponenten, erwartet wird 0
+        assertEquals("\nEs sollte 0 ausgegeben werden, es wurde aber " +
+                        potency.calculatePotency(2, -1) + " ausgegeben",
+                0, potency.calculatePotency(2, -1));
+        assertEquals("\nEs sollte 0 ausgegeben werden, es wurde aber " +
+                        potency.calculatePotency(5, -3) + " ausgegeben",
+                0, potency.calculatePotency(5, -3));
     }
 
     @Test
     public void testCalculatePotencyWithBaseZero() {
         Potency potency = new Potency();
-        assertEquals(0, potency.calculatePotency(0, 1));  // Test für 0^1 = 0
-        assertEquals(0, potency.calculatePotency(0, 5));  // Test für 0^n = 0, n > 0
+        assertEquals("\nEs sollte 0 ausgegeben werden, es wurde aber " +
+                        potency.calculatePotency(0, 1) + " ausgegeben",
+                0, potency.calculatePotency(0, 1));
+        assertEquals("\nEs sollte 0 ausgegeben werden, es wurde aber " +
+                        potency.calculatePotency(0, 5) + " ausgegeben",
+                0, potency.calculatePotency(0, 5));
     }
 
     @Test
     public void testCalculatePotencyWithExponentZero() {
         Potency potency = new Potency();
-        assertEquals(1, potency.calculatePotency(3, 0));  // Test für a^0 = 1, a > 0
-        assertEquals(1, potency.calculatePotency(10, 0)); // Test für a^0 = 1, a > 0
-        assertEquals(1, potency.calculatePotency(0, 0));  // Test für 0^0, normalerweise 1
+        assertEquals("\nEs sollte 1 ausgegeben werden, es wurde aber " +
+                        potency.calculatePotency(3, 0) + " ausgegeben",
+                1, potency.calculatePotency(3, 0));
+        assertEquals("\nEs sollte 1 ausgegeben werden, es wurde aber " +
+                        potency.calculatePotency(10, 0) + " ausgegeben",
+                1, potency.calculatePotency(10, 0));
+        assertEquals("\nEs sollte 1 ausgegeben werden, es wurde aber " +
+                        potency.calculatePotency(0, 0) + " ausgegeben",
+                1, potency.calculatePotency(0, 0));
     }
 
     @Test
     public void testCalculatePotencyWithLargeNumbers() {
         Potency potency = new Potency();
-        assertEquals(1024, potency.calculatePotency(2, 10));  // Test für 2^10 = 1024
-        assertEquals(59049, potency.calculatePotency(3, 10)); // Test für 3^10 = 59049
+        assertEquals("\nEs sollte 1024 ausgegeben werden, es wurde aber " +
+                        potency.calculatePotency(2, 10) + " ausgegeben",
+                1024, potency.calculatePotency(2, 10));
+        assertEquals("\nEs sollte 59049 ausgegeben werden, es wurde aber " +
+                        potency.calculatePotency(3, 10) + " ausgegeben",
+                59049, potency.calculatePotency(3, 10));
     }
 }
